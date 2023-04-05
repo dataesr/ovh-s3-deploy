8850dbd3705283b4b3fdc264f817056df4b30f73
folland87 <frederic@olland.me>
2023-04-05 12:58:18 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,8 @@ then
   echo "Bucket $BUCKET already exists"
 else
   echo "Creating bucket $BUCKET"
-  aws s3 mb s3://$BUCKET --acl public-read
+  aws s3 mb s3://$BUCKET
+  aws s3api put-bucket-acl --bucket $BUCKET --acl public-read
 fi
 
 aws s3 website s3://$BUCKET --error-document index.html --index-document index.html
